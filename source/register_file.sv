dedd824fd3ea1640d2d7fa7cb83b4a8bbc5abc45
sransara <git@sransara.com>
2014-08-28 19:02:32 -0400

    Register file

---
+++ source/register_file.sv
@@ -0,0 +1,28 @@
+`include "register_file_if.vh"
+`include "cpu_types_pkg.vh"
+
+module register_file
+import cpu_types_pkg::word_t;
+(
+  input logic CLK, nRST,
+  register_file_if.rf rfif
+);
+
+word_t register[31:0];
+
+always_ff @(posedge CLK, negedge nRST)
+begin
+  if(!nRST)
+  begin
+    register <= '{default:32'b0};
+  end
+  else if(rfif.WEN)
+  begin
+    register[rfif.wsel] <= rfif.wdat;
+  end
+end
+
+assign rfif.rdat1 = (rfif.rsel1) == 0 ? 0 : register[rfif.rsel1];
+assign rfif.rdat2 = (rfif.rsel2) == 0 ? 0 : register[rfif.rsel2];
+
+endmodule
--- testbench/register_file_tb.sv
+++ testbench/register_file_tb.sv
@@ -17,18 +17,13 @@ module register_file_tb;
 
   logic CLK = 0, nRST;
 
-  // test vars
-  int v1 = 1;
-  int v2 = 4721;
-  int v3 = 25119;
-
   // clock
   always #(PERIOD/2) CLK++;
 
   // interface
   register_file_if rfif ();
   // test program
-  test PROG ();
+  test #(.PERIOD (PERIOD)) PROG (CLK, nRST, rfif.rf);
   // DUT
 `ifndef MAPPED
   register_file DUT(CLK, nRST, rfif);
@@ -48,5 +43,71 @@ module register_file_tb;
 
 endmodule
 
-program test;
+program test
+(
+  input logic CLK,
+  output logic nRST,
+  register_file_if.rf rf
+);
+  parameter PERIOD = 10;
+  integer i;
+
+  initial
+  begin
+    nRST = 0;
+    rf.rsel1 = 10;
+    rf.rsel2 = 5;
+    #(PERIOD)
+    $monitor("@%00g CLK = %b nRST = %b WRITE: %0d:%0d READ: %0d:%0d %0d:%0d", $time, CLK, nRST, rf.wsel, rf.wdat, rf.rsel1, rf.rdat1, rf.rsel2, rf.rdat2);
+    #(PERIOD)
+
+    nRST = 1;
+    rf.WEN = 1;
+    rf.wsel = 0;
+    rf.wdat = 22;
+    rf.rsel1 = 0;
+    rf.rsel2 = 0;
+    #(PERIOD)
+    $monitor("@%00g CLK = %b nRST = %b WRITE: %0d:%0d READ: %0d:%0d %0d:%0d", $time, CLK, nRST, rf.wsel, rf.wdat, rf.rsel1, rf.rdat1, rf.rsel2, rf.rdat2);
+
+    rf.wsel = 5;
+    rf.wdat = 123;
+    rf.rsel1 = 5;
+    rf.rsel2 = 5;
+    #(PERIOD)
+    $monitor("@%00g CLK = %b nRST = %b WRITE: %0d:%0d READ: %0d:%0d %0d:%0d", $time, CLK, nRST, rf.wsel, rf.wdat, rf.rsel1, rf.rdat1, rf.rsel2, rf.rdat2);
+
+    rf.wsel = 31;
+    rf.wdat = 321;
+    rf.rsel1 = 5;
+    rf.rsel2 = 31;
+    #(PERIOD)
+    $monitor("@%00g CLK = %b nRST = %b WRITE: %0d:%0d READ: %0d:%0d %0d:%0d", $time, CLK, nRST, rf.wsel, rf.wdat, rf.rsel1, rf.rdat1, rf.rsel2, rf.rdat2);
+
+    $monitor("@FOR WRITE LOOP INCOMING");
+    for(i = 0; i < 32; i = i + 1)
+    begin
+      if(i % 10 == 0)
+        rf.WEN = 0;
+      else
+        rf.WEN = 1;
+      rf.wsel = i;
+      rf.wdat = (2 ** i) - 1;
+      rf.rsel1 = i;
+      rf.rsel2 = i;
+      #(PERIOD)
+      $monitor("@%00g CLK = %b nRST = %b WRITE: %0d:%0d READ: %0d:%0d %0d:%0d", $time, CLK, nRST, rf.wsel, rf.wdat, rf.rsel1, rf.rdat1, rf.rsel2, rf.rdat2);
+    end
+
+    $monitor("@FOR READ LOOP INCOMING");
+    for(i = 0; i < 32; i = i + 2)
+    begin
+      rf.rsel1 = i;
+      rf.rsel2 = i + 1;
+      #(PERIOD)
+      $monitor("@%00g CLK = %b nRST = %b WRITE: %0d:%0d READ: %0d:%0d %0d:%0d", $time, CLK, nRST, rf.wsel, rf.wdat, rf.rsel1, rf.rdat1, rf.rsel2, rf.rdat2);
+    end
+
+    $finish;
+  end
 endprogram
